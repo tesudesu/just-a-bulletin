540ff55580831342fc4fc0fdadaecaaa36255764
Te Su <sute515@gmail.com>
2023-03-19 16:28:30 -0700

    rewrote in react functional components

--- client/src/index.js
+++ client/src/index.js
@@ -1,90 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
 const { DateTime } = require('luxon');
 
 const rootUri = process.env.REACT_APP_SERVER_URL || 'http://localhost:9000'
 
-class Message extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            board: '',
-            user: '',
-            text: '',
-            userSearch: '',
-            dateFromSearch: '',
-            dateToSearch: '',
-            dataIsLoaded: false,
-        }
-        this.handleChangeUser = this.handleChangeUser.bind(this);
-        this.handleChangeText = this.handleChangeText.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChangeUserSearch = this.handleChangeUserSearch.bind(this);
-        this.handleChangeDateFromSearch = this.handleChangeDateFromSearch.bind(this);
-        this.handleChangeDateToSearch = this.handleChangeDateToSearch.bind(this);
-        this.handleFind = this.handleFind.bind(this);
-        this.handleAll = this.handleAll.bind(this);
-    }
+const Message = () => {
+    const [board, setBoard] = useState('');
+    const [user, setUser] = useState('');
+    const [text, setText] = useState('');
+    const [userSearch, setUserSearch] = useState('');
+    const [dateFromSearch, setDateFromSearch] = useState('');
+    const [dateToSearch, setDateToSearch] = useState('');
+    const [dataIsLoaded, setDataIsLoaded] = useState(false);
 
-    componentDidMount() {
+    useEffect(() => {
         fetch(`${rootUri}/api`)
             .then(res => res.json())
-            .then(res => this.setState({ board: res, dataIsLoaded: true }))
+            .then(res => {
+                setBoard(res);
+                setDataIsLoaded(true);
+            })
             .catch(err => err);
+    }, []);
+
+    const handleChangeUser = (e) => {
+        setUser(e.target.value);
     };
 
-    handleChangeUser(e) {
-        this.setState({
-            user: e.target.value
-        });
+    const handleChangeText = (e) => {
+        setText(e.target.value);
     };
 
-    handleChangeText(e) {
-        this.setState({
-            text: e.target.value
-        });
-    };
-
-    handleSubmit(e) {
-        e.preventDefault()
+    const handleSubmit = (e) => {
+        e.preventDefault();
         fetch(`${rootUri}/new`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ user: this.state.user, text: this.state.text }),
+            body: JSON.stringify({ user: user, text: text }),
         })
             .then(res => res.json())
-            .then(res => this.setState({
-                board: res,
-                user: '',
-                text: ''
-            }));
+            .then(res => {
+                setBoard(res);
+                setUser('');
+                setText('');
+            });
         console.log('submit');
     };
 
-    handleChangeUserSearch(e) {
-        this.setState({
-            userSearch: e.target.value
-        });
+    const handleChangeUserSearch = (e) => {
+        setUserSearch(e.target.value);
     };
 
-    handleChangeDateFromSearch(e) {
-        this.setState({
-            dateFromSearch: e.target.value
-        });
+    const handleChangeDateFromSearch = (e) => {
+        setDateFromSearch(e.target.value);
     };
 
-    handleChangeDateToSearch(e) {
-        this.setState({
-            dateToSearch: e.target.value
-        });
+    const handleChangeDateToSearch = (e) => {
+        setDateToSearch(e.target.value);
     };
 
-    handleFind() {
-        if (this.state.userSearch === '' && this.state.dateFromSearch === '' && this.state.dateToSearch === '') {
+    const handleFind = () => {
+        if (!userSearch && !dateFromSearch && !dateToSearch) {
             fetch(`${rootUri}/api`)
                 .then(res => res.json())
-                .then(res => this.setState({ board: res }))
+                .then(res => setBoard(res))
                 .catch(err => err);
             return;
         }
@@ -92,125 +72,124 @@ class Message extends React.Component {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-                usersearch: this.state.userSearch,
-                datefromsearch: this.state.dateFromSearch,
-                datetosearch: this.state.dateToSearch
+                usersearch: userSearch,
+                datefromsearch: dateFromSearch,
+                datetosearch: dateToSearch
             }),
         })
             .then(res => res.json())
-            .then(res => this.setState({
-                board: res
-            }));
+            .then(res => setBoard(res));
         console.log('searched');
     };
 
-    handleAll() {
+    const handleAll = () => {
         fetch(`${rootUri}/api`)
             .then(res => res.json())
-            .then(res => this.setState({ board: res }))
+            .then(res => {
+                setBoard(res);
+                setUserSearch('');
+                setDateFromSearch('');
+                setDateToSearch('');
+            })
             .catch(err => err);
-        this.setState({
-            userSearch: '',
-            dateFromSearch: '',
-            dateToSearch: ''
-        });
     };
 
-    render() {
-        if (!this.state.dataIsLoaded) {
-            return <div><h1>Please wait some time...</h1></div>
+    if (!dataIsLoaded) {
+        return <div><h1>Please wait some time...</h1></div>
+    };
+
+    let bulletin = board.messages;
+    bulletin.sort((b, a) => {
+        if (new Date(a.date) < new Date(b.date)) {
+            return -1;
+        } else if (new Date(a.date) > new Date(b.date)) {
+            return 1;
+        } else {
+            return 0;
         }
-        let board = this.state.board.messages;
-        board.sort((b, a) => {
-            if (new Date(a.date) < new Date(b.date)) {
-                return -1;
-            } else if (new Date(a.date) > new Date(b.date)) {
-                return 1;
-            } else {
-                return 0;
-            }
-        });
-        let count = this.state.board.messages.length;
-        const charAllowed = 500;
-        let charRemain = charAllowed - this.state.text.length;
-        let singularOrPlural = count > 1 ? 'messages' : 'message';
+    });
 
-        return (
-            <div>
-                <h1>Just A Bulletin</h1>
-                <h4>Write a message for strangers across the globe or beside you</h4>
-                <div id='container'>
-                    <div id='left'>
-                        <form onSubmit={this.handleSubmit}>
-                            <h3>Post a New Message</h3>
-                            <p>
-                                <label className='label2'>What do you call yourself?</label>
-                            </p>
-                            <p>
-                                <input required type='text' className='input-box' maxLength='30' size='20' value={this.state.user} onChange={this.handleChangeUser} />
-                            </p>
-                            <p>
-                                <label className='label2'>What do you want to say?</label>
-                            </p>
-                            <div>
-                                <textarea required className='message-box' rows="4" maxLength={charAllowed} value={this.state.text} onChange={this.handleChangeText} /><br />
-                            </div>
-                            <div id='char-remaining'>{charRemain} characters remaining</div>
-                            <p className='button'>
-                                <input className='button-style' type='submit' value='Submit'/>
-                            </p>
-                        </form>
+    const count = board.messages.length;
+    const charAllowed = 500;
+    const charRemain = charAllowed - text.length;
+    const singularOrPlural = count > 1 ? 'messages' : 'message';
 
+    return (
+        <div>
+            <h1>Just A Bulletin</h1>
+            <h4>Write a message for strangers across the globe or beside you</h4>
+            <div id='container'>
+                <div id='left'>
+                    <form onSubmit={handleSubmit}>
+                        <h3>Post a New Message</h3>
+                        <p>
+                            <label className='label2'>What do you call yourself?</label>
+                        </p>
+                        <p>
+                            <input required type='text' className='input-box' maxLength='30' size='20' value={user} onChange={handleChangeUser} />
+                        </p>
+                        <p>
+                            <label className='label2'>What do you want to say?</label>
+                        </p>
                         <div>
-                            <h3>Find Messages by Alias and/or Date</h3>
-                            <p>
-                                <label className='label2'>Alias: </label>
-                                <input type='text' className='input-box' maxLength='30' size='20' placeholder='case-sensitive' value={this.state.userSearch} onChange={this.handleChangeUserSearch} />
-                            </p>
-                            <div className='spaceout'>
-                                <div>
-                                    <label className='label2'>From: </label>
-                                    <input type='date' className='input-box' value={this.state.dateFromSearch} onChange={this.handleChangeDateFromSearch} />
-                                </div>
-                                <div>
-                                    <label className='label2'>To: </label>
-                                    <input type='date' className='input-box' value={this.state.dateToSearch} onChange={this.handleChangeDateToSearch} />
-                                </div>
+                            <textarea required className='message-box' rows="4" maxLength={charAllowed} value={text} onChange={handleChangeText} /><br />
+                        </div>
+                        <div id='char-remaining'>{charRemain} characters remaining</div>
+                        <p className='button'>
+                            <input className='button-style' type='submit' value='Submit' />
+                        </p>
+                    </form>
+
+                    <div>
+                        <h3>Find Messages by Alias and/or Date</h3>
+                        <p>
+                            <label className='label2'>Alias: </label>
+                            <input type='text' className='input-box' maxLength='30' size='20' placeholder='case-sensitive' value={userSearch} onChange={handleChangeUserSearch} />
+                        </p>
+                        <div className='spaceout'>
+                            <div>
+                                <label className='label2'>From: </label>
+                                <input type='date' className='input-box' value={dateFromSearch} onChange={handleChangeDateFromSearch} />
                             </div>
-                            <p className='button'>
-                                <button className='button-style' onClick={this.handleFind}>Search</button>
-                            </p>
-                            <p className='button'>
-                                <button className='button-style' onClick={this.handleAll}>All Messages</button>
-                            </p>
-                            <p id='all-messages'>
-                                <span className='label'>{count}</span> <span className='label2'>{singularOrPlural} on screen</span>
-                            </p>
+                            <div>
+                                <label className='label2'>To: </label>
+                                <input type='date' className='input-box' value={dateToSearch} onChange={handleChangeDateToSearch} />
+                            </div>
                         </div>
+                        <p className='button'>
+                            <button className='button-style' onClick={handleFind}>Search</button>
+                        </p>
+                        <p className='button'>
+                            <button className='button-style' onClick={handleAll}>All Messages</button>
+                        </p>
+                        <p id='all-messages'>
+                            <span className='label'>{count}</span> <span className='label2'>{singularOrPlural} on screen</span>
+                        </p>
                     </div>
+                </div>
 
-                    <div id='right'>
-                        <div id='message-board'>
-                            {board.map((message, ind) => (
-                                <div className='message' key={ind}>
-                                    <div className='spaceout'>
-                                        <div><span className='label'>Alias: </span>{message.user}</div>
-                                        <div><span className='label'>Posted: </span>{DateTime.fromJSDate(new Date(message.date)).toLocaleString(DateTime.DATETIME_MED_WITH_WEEKDAY)}</div>
-                                    </div>
-                                    <p>{message.text}</p>
+                <div id='right'>
+                    <div id='message-board'>
+                        {bulletin.map((message, ind) => (
+                            <div className='message' key={ind}>
+                                <div className='spaceout'>
+                                    <div><span className='label'>Alias: </span>{message.user}</div>
+                                    <div><span className='label'>Posted: </span>{DateTime.fromJSDate(new Date(message.date)).toLocaleString(DateTime.DATETIME_MED_WITH_WEEKDAY)}</div>
                                 </div>
-                            ))}
-                        </div>
+                                <p>{message.text}</p>
+                            </div>
+                        ))}
                     </div>
+                </div>
 
-                </div>
-                <div id='footer'>
-                    <a href='mailto:witty.opposum@gmail.com'>© Te Su</a>
-                </div>
             </div>
-        );
-    }
+            <div id='footer'>
+                <a href='mailto:witty.opposum@gmail.com'>© Te Su</a>
+            </div>
+        </div>
+    );
 }
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Message />);
